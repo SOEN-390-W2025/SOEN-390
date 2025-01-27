07352e6b673fafbf216c2a6982042cdf00393417
Jutipong Puntuleng <156547682+Jpuntul@users.noreply.github.com>
2025-01-27 15:38:01 -0500

    Implement reusable AppBar widget for homepage

--- concordia_nav/lib/view/homepage_view.dart
+++ concordia_nav/lib/view/homepage_view.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:concordia_nav/widgets/custom_appbar.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('HomePage'),
-      ),
+      appBar: customAppBar(context, 'Home'),
       body: Center(
         child: Text('HomePage'),
       ),
