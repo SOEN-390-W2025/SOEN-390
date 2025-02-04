fc70b75c30506e723258c7fa22e86975456edfa2
Jutipong Puntuleng <156547682+Jpuntul@users.noreply.github.com>
2025-02-04 07:42:41 -0500

    Implement navigation to POI map after facility selection

---
+++ concordia_nav/lib/core/ui/widgets/poi_map_view.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'custom_appbar.dart';
+import 'map_layout.dart';
+
+class POIMapView extends StatelessWidget {
+  const POIMapView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: customAppBar(context, 'Nearby Facility'),
+      body: Stack(
+        children: [
+          MapLayout(
+            searchController: TextEditingController(),
+            mapWidget: Container(),
+          ),
+          Positioned(
+            top: 10,
+            left: 15,
+            right: 15,
+            child: Center(
+              child: Container(
+                height: 40.0,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 12.0,
+                    vertical: 8.0,
+                  ),
+                  child: Text(
+                    'Nearby Facility', // Placeholder text
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 90.0,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 12.0,
+                vertical: 8.0,
+              ),
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 240, 240, 240),
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Time', // Placeholder text
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    'Distance', // Placeholder text
+                    style: const TextStyle(
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
