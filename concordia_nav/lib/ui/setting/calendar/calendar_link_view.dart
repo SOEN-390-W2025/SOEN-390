9b9e13830407a798f361bd24d03a51d5a6528096
Jutipong Puntuleng <156547682+Jpuntul@users.noreply.github.com>
2025-02-07 14:39:25 -0500

    Update link function to navigate to calendar placeholder page if permission is granted

--- concordia_nav/lib/ui/setting/calendar/calendar_link_view.dart
+++ concordia_nav/lib/ui/setting/calendar/calendar_link_view.dart
@@ -1,28 +1,59 @@
 import 'package:flutter/material.dart';
+import '../../../data/repositories/calendar.dart';
 import '../../../widgets/custom_appbar.dart';
+import 'calandar_view.dart';
 
 class CalendarLinkView extends StatelessWidget {
   const CalendarLinkView({super.key});
 
+  /// Request calendar permissions.
+  Future<void> requestCalendarPermissions(BuildContext context) async {
+    // Check if permission is granted
+    final permissionsGranted = await CalendarRepository().checkPermissions();
+    
+    if (context.mounted) {
+      if (permissionsGranted) {
+        // Assume there is no error
+        // Navigate to calendar view
+        await Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const CalendarView()),
+        );
+
+      } else {
+        // Show an error if permission is denied
+
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Permission denied. Please enable it in settings.')),
+        );
+      }
+    }
+  }
+
+  // This shows a UI that allows the user to request calendar permissions.
+  // If permission is granted, it navigates to the calendar view. If permission
+  // is denied, it shows an error.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(context, 'Calendar Link'),
       body: Padding(
-        padding: const EdgeInsets.all(16.0), 
+        padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            // Show a message if there is no calendar
             const Text(
               'There is currently no calendar',
               style: TextStyle(
                 fontSize: 16,
-              )
+              ),
             ),
             const SizedBox(height: 16),
+            // Show a button to request calendar permissions
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () => requestCalendarPermissions(context),
               style: ElevatedButton.styleFrom(
                 backgroundColor: Theme.of(context).colorScheme.secondary,
                 shape: RoundedRectangleBorder(
