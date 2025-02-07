593c38103337db043ffc6d2100953fbddd95b4b0
Jutipong Puntuleng <156547682+Jpuntul@users.noreply.github.com>
2025-02-07 14:13:30 -0500

    Create calendar link page with link button

--- concordia_nav/lib/ui/setting/calendar/calendar_link_view.dart
+++ concordia_nav/lib/ui/setting/calendar/calendar_link_view.dart
@@ -8,14 +8,37 @@ class CalendarLinkView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(context, 'Calendar Link'),
-      body: Column(
-        children: [
-          const Text('There is currently no calendar'),
-          const SizedBox(height: 16),
-          ElevatedButton(
-            onPressed: () {},
-            child: const Text('Link'),)
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0), 
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'There is currently no calendar',
+              style: TextStyle(
+                fontSize: 16,
+              )
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.secondary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+              ),
+              child: const Text(
+                'Link',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.black,
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
