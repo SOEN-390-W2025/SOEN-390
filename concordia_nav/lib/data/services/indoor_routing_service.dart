e00c5f6690fe2a176ccfaf939dbe94028fbac8fc
Brian Tkatch <brian@briantkatch.com>
2025-02-11 16:18:05 -0500

    Add rounded geolocation method

--- concordia_nav/lib/data/services/indoor_routing_service.dart
+++ concordia_nav/lib/data/services/indoor_routing_service.dart
@@ -1,3 +1,80 @@
+import 'package:geolocator/geolocator.dart';
+import '../domain-model/concordia_building.dart';
+import '../domain-model/concordia_campus.dart';
+import '../domain-model/location.dart';
+import '../repositories/building_repository.dart';
+
 class IndoorRoutingService {
   //IndoorRoute getRoute(Location origin, Location destination) {}
+
+  static const roundingMinimumProximityMeters = 25.0;
+
+  /// Returns at minimum a Location object with latitide and longitude based on
+  /// the device location. If the device is within the minimum rounding
+  /// proximity of a ConcordiaBuilding, it will return that ConcordiaBuilding
+  /// which is closest to the user.
+  ///
+  /// Returns null if permission is denied or location services are not
+  /// available.
+  static Future<Location?> getRoundedLocation() async {
+    List<ConcordiaBuilding>? searchCandidates;
+    final Position userPosition;
+
+    try {
+      await Geolocator.requestPermission();
+      userPosition = await Geolocator.getCurrentPosition(
+          locationSettings: const LocationSettings(
+              accuracy: LocationAccuracy.bestForNavigation));
+    } on Exception {
+      return null;
+    }
+
+    // Discard location if wildly inaccurate
+    if (userPosition.accuracy > 50.0) return null;
+
+    // Search through the appropriate list of buildings if the user is close to
+    // either campus
+    if (Geolocator.distanceBetween(
+            ConcordiaCampus.sgw.lat,
+            ConcordiaCampus.sgw.lng,
+            userPosition.latitude,
+            userPosition.longitude) <
+        1000) {
+      searchCandidates = BuildingRepository
+          .buildingByCampusAbbreviation[ConcordiaCampus.sgw.abbreviation];
+    } else if (Geolocator.distanceBetween(
+            ConcordiaCampus.loy.lat,
+            ConcordiaCampus.loy.lng,
+            userPosition.latitude,
+            userPosition.longitude) <
+        1000) {
+      searchCandidates = BuildingRepository
+          .buildingByCampusAbbreviation[ConcordiaCampus.loy.abbreviation];
+    }
+
+    ConcordiaBuilding? bestCandidate;
+    double bestCandidateDistance = double.infinity;
+
+    if (searchCandidates != null) {
+      for (final candidateBuilding in searchCandidates) {
+        final double distance = Geolocator.distanceBetween(
+            candidateBuilding.lat,
+            candidateBuilding.lng,
+            userPosition.latitude,
+            userPosition.longitude);
+        if (distance < roundingMinimumProximityMeters &&
+            distance < bestCandidateDistance) {
+          bestCandidate = candidateBuilding;
+          bestCandidateDistance = distance;
+        }
+      }
+    }
+
+    if (bestCandidate != null) {
+      return bestCandidate;
+    } else {
+      return Location(userPosition.latitude, userPosition.longitude,
+          "Current Location", null, null, null, null);
+    }
+  }
 }
