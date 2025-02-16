67ee6cc5aca8d2c4dcc1d2719a7dee400a449423
Jutipong Puntuleng <156547682+Jpuntul@users.noreply.github.com>
2025-02-16 17:20:45 -0500

    Add comments to related code

--- concordia_nav/lib/utils/splash_screen_viewmodel.dart
+++ concordia_nav/lib/utils/splash_screen_viewmodel.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_final_locals, use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import '../data/domain-model/concordia_campus.dart';
@@ -6,20 +8,27 @@ import 'map_viewmodel.dart';
 class SplashScreenViewModel {
   final MapViewModel _mapViewModel = MapViewModel();
 
+  /// Navigates to the closest campus map if the user is within 1km of either campus,
+  /// otherwise navigates to the home page.
   Future<void> navigateBasedOnLocation(BuildContext context) async {
     try {
       bool hasAccess = await _mapViewModel.checkLocationAccess();
       if (!hasAccess) {
+        // Navigate to home page if the user has denied location access
         await _navigateToHome(context);
         return;
       }
 
+      // Fetch the user's current location
       LatLng? currentLocation = await _mapViewModel.fetchCurrentLocation();
       if (currentLocation == null) {
+        // Navigate to home page if the user's location cannot be determined
         await _navigateToHome(context);
         return;
       }
 
+      // Determine the distance between the user's current location
+      // and both campuses
       double distanceToSGW = _mapViewModel.getDistance(
         currentLocation,
         LatLng(ConcordiaCampus.sgw.lat, ConcordiaCampus.sgw.lng),
@@ -29,6 +38,8 @@ class SplashScreenViewModel {
         LatLng(ConcordiaCampus.loy.lat, ConcordiaCampus.loy.lng),
       );
 
+      // Navigate to the closest campus map if the user is within 1km
+      // of either campus, otherwise navigate to the home page
       if (distanceToSGW < 1000) {
         await _navigateToCampus(context, ConcordiaCampus.sgw);
       } else if (distanceToLOY < 1000) {
@@ -38,15 +49,20 @@ class SplashScreenViewModel {
       }
     } catch (e) {
       print('Error determining location: $e');
+      // Navigate to home page if there is an error determining the user's location
       await _navigateToHome(context);
     }
   }
 
+  /// Navigates to the specified campus map page.
   Future<void> _navigateToCampus(BuildContext context, ConcordiaCampus campus) async {
+    // Push a replacement route to the CampusMapPage with the specified campus
     await Navigator.pushReplacementNamed(context, '/CampusMapPage', arguments: campus);
   }
 
+  /// Navigates to the home page.
   Future<void> _navigateToHome(BuildContext context) async {
+    // Push a replacement route to the HomePage
     await Navigator.pushReplacementNamed(context, '/HomePage');
   }
 }
