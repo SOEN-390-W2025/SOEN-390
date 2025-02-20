50c8360098d0dd0e1267e1634035d2ee1b153317
Nirav Patel <niravp0703@gmail.com>
2025-02-19 20:38:31 -0500

    fix: sonarqube code smell

--- concordia_nav/lib/widgets/custom_appbar.dart
+++ concordia_nav/lib/widgets/custom_appbar.dart
@@ -4,48 +4,55 @@ import 'package:flutter/material.dart';
 PreferredSizeWidget customAppBar(BuildContext context, String title,
     {Icon? actionIcon, Function? onActionPressed}) {
   final isHomePage = ModalRoute.of(context)?.settings.name == '/HomePage';
-  final isCampusMapPage = ModalRoute.of(context)?.settings.name == '/CampusMapPage';
+  final isCampusMapPage =
+      ModalRoute.of(context)?.settings.name == '/CampusMapPage';
+
+  Widget leadingIcon;
+
+  if (isHomePage) {
+    leadingIcon = IconButton(
+      onPressed: () {
+        Navigator.pushNamed(context, '/SettingsPage');
+      },
+      icon: const Icon(
+        Icons.settings,
+        color: Colors.white,
+      ),
+    );
+  } else if (isCampusMapPage) {
+    leadingIcon = IconButton(
+      onPressed: () {
+        Navigator.pushReplacementNamed(context, '/HomePage'); // Navigate back
+      },
+      icon: const Icon(
+        Icons.arrow_back,
+        color: Colors.white,
+      ),
+    );
+  } else {
+    leadingIcon = IconButton(
+      onPressed: () {
+        Navigator.pop(context); // Navigate back
+      },
+      icon: const Icon(
+        Icons.arrow_back,
+        color: Colors.white,
+      ),
+    );
+  }
 
   return AppBar(
     backgroundColor: Theme.of(context).primaryColor,
     title: FittedBox(
       fit: BoxFit.fitWidth,
-      child: Text(
-        title,
-        style: const TextStyle(
-          color: Colors.white,
-          fontSize: 20,
-        )
-      ),
+      child: Text(title,
+          style: const TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+          )),
     ),
     centerTitle: true,
-    leading: isHomePage
-        ? IconButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/SettingsPage');
-          },
-          icon: const Icon(
-            Icons.settings,
-            color: Colors.white,
-          ),
-        ) : isCampusMapPage 
-        ? IconButton(
-          onPressed: () {
-            Navigator.pushReplacementNamed(context, '/HomePage'); // Navigate back
-          },
-          icon: const Icon(
-            Icons.arrow_back,
-            color: Colors.white,
-          ),
-        ) : IconButton(
-            onPressed: () {
-              Navigator.pop(context); // Navigate back
-            },
-            icon: const Icon(
-              Icons.arrow_back,
-              color: Colors.white,
-            ),
-          ),
+    leading: leadingIcon,
     actions: [
       IconButton(
         onPressed: () {
