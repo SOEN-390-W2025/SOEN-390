37d194550a4806029d25cfb061b36d9e4779fb87
Jutipong Puntuleng <156547682+Jpuntul@users.noreply.github.com>
2025-03-24 13:46:50 -0400

    Rename 'Indoor Direction' to 'Floor Navigation'

--- concordia_nav/lib/ui/home/homepage_view.dart
+++ concordia_nav/lib/ui/home/homepage_view.dart
@@ -85,7 +85,7 @@ class HomePage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   FeatureCard(
-                    title: 'Indoor directions',
+                    title: 'Floor Navigation',
                     icon: const Icon(Icons.meeting_room),
                     onPress: () =>
                         Navigator.pushNamed(context, '/BuildingSelection'),
