5ea92727b9a9e7127420b13dc31d95308534d05e
Jutipong Puntuleng <156547682+Jpuntul@users.noreply.github.com>
2025-03-25 18:10:10 -0400

    Modify color theme in customAppbar

--- concordia_nav/lib/widgets/custom_appbar.dart
+++ concordia_nav/lib/widgets/custom_appbar.dart
@@ -7,6 +7,10 @@ PreferredSizeWidget customAppBar(BuildContext context, String title,
   final isCampusMapPage =
       ModalRoute.of(context)?.settings.name == '/CampusMapPage';
 
+  // Get theme colors
+  final primaryColor = Theme.of(context).primaryColor;
+  final onPrimaryColor = Theme.of(context).colorScheme.onPrimary;
+  
   Widget leadingIcon;
 
   if (isHomePage) {
@@ -14,9 +18,9 @@ PreferredSizeWidget customAppBar(BuildContext context, String title,
       onPressed: () {
         Navigator.pushNamed(context, '/SettingsPage');
       },
-      icon: const Icon(
+      icon: Icon(
         Icons.settings,
-        color: Colors.white,
+        color: onPrimaryColor,
       ),
     );
   } else if (isCampusMapPage) {
@@ -24,9 +28,9 @@ PreferredSizeWidget customAppBar(BuildContext context, String title,
       onPressed: () {
         Navigator.pushReplacementNamed(context, '/HomePage'); // Navigate back
       },
-      icon: const Icon(
+      icon: Icon(
         Icons.arrow_back,
-        color: Colors.white,
+        color: onPrimaryColor,
       ),
     );
   } else {
@@ -34,9 +38,9 @@ PreferredSizeWidget customAppBar(BuildContext context, String title,
       onPressed: () {
         Navigator.pop(context); // Navigate back
       },
-      icon: const Icon(
+      icon: Icon(
         Icons.arrow_back,
-        color: Colors.white,
+        color: onPrimaryColor,
       ),
     );
   }
@@ -55,14 +59,16 @@ PreferredSizeWidget customAppBar(BuildContext context, String title,
         ],
       ),
       child: AppBar(
-        backgroundColor: Theme.of(context).primaryColor,
+        backgroundColor: primaryColor,
         title: FittedBox(
           fit: BoxFit.fitWidth,
-          child: Text(title,
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 20,
-              )),
+          child: Text(
+            title,
+            style: TextStyle(
+              color: onPrimaryColor,
+              fontSize: 20,
+            ),
+          ),
         ),
         centerTitle: true,
         leading: leadingIcon,
@@ -77,9 +83,9 @@ PreferredSizeWidget customAppBar(BuildContext context, String title,
               }
             },
             icon: actionIcon ??
-                const Icon(
+                Icon(
                   Icons.edit_note,
-                  color: Colors.white,
+                  color: onPrimaryColor,
                 ),
           ),
         ],
